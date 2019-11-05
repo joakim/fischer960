fdeb359faf3a281468a31c1878d6dd00df6e51f6
Joakim Stai <joakimstai@gmail.com>
2019-11-05 02:32:51 +0100

    Add ability to build all the things!

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import buble from '@rollup/plugin-buble'
+import pkg from './package.json'
+
+const pluginsUMD = [
+  replace({ 'process.browser': true }),
+  resolve(),
+  commonjs(),
+  buble({ exclude: 'node_modules/**' }),
+]
+
+export default [
+  // Complete browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    output: {
+      name: 'Fischer960',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: pluginsUMD,
+  },
+  // Server-friendly CommonJS (for Node) and ES module (for bundlers) build
+  {
+    input: 'src/main.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      replace({ 'process.browser': false }),
+      buble({ exclude: 'node_modules/**' }),
+    ],
+  },
+]
